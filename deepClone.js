496a95a8ba57bdeca911e9899b4b2180347280f6
Fendy <281966728@qq.com>
2018-04-13 16:47:11 +0800

    Promise 完成
    Signed-off-by: fendy <281966728@qq.com>

---
+++ deepClone.js
@@ -0,0 +1,12 @@
+/**
+ * 深度克隆
+ * */
+
+function extend() {
+    return Array.from(arguments).reduce(clone);
+}
+
+
+function clone(target, obj) {
+    
+}
--- promise.js
+++ promise.js
@@ -4,6 +4,7 @@
 
 /**
  * Reconstruction by 2018-04-07
+ * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise
  * */
 
 const Pending = 'pending';
@@ -11,29 +12,120 @@ const Resolve = 'resolve';
 const Reject = 'reject';
 
 function PromiseF(callback) {
+    let self = this;
     this.state = Pending;
-    callback();
+    this.msg = '';
+
+    function resolve(msg) {
+        if (self.state === Pending) {
+            self.state = Resolve;
+            self.msg = msg;
+        } else if (self.state === Reject) {
+            throw '[error]res, rej不可同时调用';
+        }
+    }
+
+    function reject(msg) {
+        if (self.state === Pending) {
+            self.state = Reject;
+            self.msg = msg;
+        } else if (self.state === Resolve) {
+            throw '[error]res，rej不可同时调用';
+        }
+    }
+
+    try {
+        setTimeout(() => {
+            callback(resolve, reject);
+        }, 0);
+    } catch (e) {
+        reject(e);
+    }
 }
 
 PromiseF.prototype = {
     then(res, rej) {
-        this.state === Reject ? res() : rej();
-        return new Promise();
+        res = typeof res === 'function' ? res : function (msg) {};
+        rej = typeof rej === 'function' ? rej : function (msg) {};
+
+        try {
+            let callback = this.state === Resolve ? res : rej;
+            let out = callback(this.msg);
+
+            if (out instanceof PromiseF) {
+                return out;
+            } else {
+                return PromiseF.resolve(out);
+            }
+        } catch (e) {
+            return PromiseF.reject(e);
+        }
     },
 
-    catch() {
+    catch(rej) {
+        return this.then(undefined, rej);
+    },
 
+    // finally的表现与then一致，只不过不区分状态
+    finally(callback) {
+        callback = typeof callback === 'function' ? callback() : callback;
+
+        try {
+            if (callback instanceof PromiseF) {
+                return callback;
+            } else {
+                return PromiseF.resolve(this.msg);
+            }
+        } catch (e) {
+            return PromiseF.reject(e);
+        }
     }
 };
 
-PromiseF.all = (array) => {
+PromiseF.all = array => {
+    if (typeof array === 'string') {
+        array = array.split('');
+    }
 
+    return new PromiseF((res, rej) => {
+        let out = [];
+        let flag = 0;
+        let is = array.every((p, index) => {
+            if (p instanceof PromiseF) {
+                let isResolved = true;
+
+                p.then(msg => {
+                    out[index] = msg;
+                    flag++;
+                }, msg => {
+                    out[index] = msg;
+                    flag++;
+                    isResolved = false;
+                });
+
+                return isResolved;
+            } else {
+                out[index] = p;
+                flag++;
+
+                return true;
+            }
+        });
+
+        if (flag >= array.length) {
+            is ? res(out) : rej(out);
+        }
+    });
 };
 
-PromiseF.resolve = () => {
-
+PromiseF.resolve = msg => {
+    return new PromiseF((res) => {
+         res(msg);
+    });
 };
 
-PromiseF.reject = () => {
-
+PromiseF.reject = msg => {
+    return new PromiseF((res, rej) => {
+        rej(msg);
+    });
 };
