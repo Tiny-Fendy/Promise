8dff8e369e284ddf6fa929a926523fff5df7efdf
Fendy <281966728@qq.com>
2018-04-17 09:19:58 +0800

    deepClone
    Signed-off-by: fendy <281966728@qq.com>

--- deepClone.js
+++ deepClone.js
@@ -8,5 +8,28 @@ function extend() {
 
 
 function clone(target, obj) {
-    
+    if (typeof target !== 'object') {
+        return false;
+    }
+
+    for (let key in obj ) {
+        if (!obj.hasOwnProperty(key) || obj[key] === obj) {
+            return true;
+        }
+
+        let val = obj[key];
+
+        if (typeof val !== 'object') {
+            target[key] = obj[key];
+        }
+
+        if (Array.isArray(val)) {
+            target[key] = [];
+            val.forEach((item, i) => {
+
+            });
+        }
+
+
+    }
 }
