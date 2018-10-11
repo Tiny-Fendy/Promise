449e8190b8b4b4de0699bd5398b9d78bd36eb7ea
fendy <281966728@qq.com>
2018-10-11 10:04:17 +0800

    创建单元测试文件
    Signed-off-by: fendy <281966728@qq.com>

---
+++ test/promise.js
@@ -0,0 +1,10 @@
+
+const assert = require('assert');
+const { expect } = require('chai');
+const Promise = require('../promise');
+
+describe('测试Promise', () => {
+    it('是个Promise对象', () => {
+
+    });
+});
